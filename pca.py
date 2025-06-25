b2e9e5129661a9d528a99d6f64df03378aed4a3a
Jinnosuke Ishizu <j.ishizu.457@stn.nitech.ac.jp>
2025-06-25 16:19:11 +0900

    change equation

--- pca.py
+++ pca.py
@@ -1,40 +1,39 @@
 import numpy as np
 import numpy.linalg as LA
 
+
 def PCA(x, y):
-  N = x.shape[0]  # データの行数
-  d = x.shape[1]  # 次元数
-  μ = np.zeros(d)   # 平均ベクトル
-  s = np.zeros((d, d))   # 分散共分散行列
-  z = np.zeros(d)    # 主成分
-  for m in range(d):
-    for i in range(N):
-      μ[m] += x[i][m] / N
-  for m in range(d):
-    for k in range(d):
-      for i in range(N):
-        s[k][m] += (x[i][k] - μ[k]) * (x[i][m] - μ[m]) / N
-  λ, v = LA.eig(s)
-  sorted_indices = np.argsort(λ)[::-1]  # 固有値を降順にソートするインデックス
-  λ = λ[sorted_indices]
-  v = v[:, sorted_indices]  # 固有ベクトルも対応して並べ替える
+    N = x.shape[0]  # データの行数
+    d = x.shape[1]  # 次元数
+    μ = np.zeros(d)   # 平均ベクトル
+    s = np.zeros((d, d))   # 分散共分散行列
+    z = np.zeros(d)    # 主成分
+    for m in range(d):
+        for i in range(N):
+            μ[m] += x[i][m] / N
+    for m in range(d):
+        for k in range(d):
+            for i in range(N):
+                s[k][m] += (x[i][k] - μ[k]) * (x[i][m] - μ[m]) / N
+    λ, v = LA.eig(s)
+    sorted_indices = np.argsort(λ)[::-1]  # 固有値を降順にソートするインデックス
+    λ = λ[sorted_indices]
+    v = v[:, sorted_indices]  # 固有ベクトルも対応して並べ替える
 
-  # 主成分スコアの計算
-  z = np.dot(x - μ, v)  # データを固有ベクトルに投影
+    # 主成分スコアの計算
+    z = np.dot(x - μ, v)  # データを固有ベクトルに投影
 
-  kiyo = np.zeros(d)
-  kiyo[y] = λ[y] / sum(λ)
+    kiyo = np.zeros(d)
+    kiyo[y] = λ[y] / sum(λ)
 
-  ruiseki = np.zeros(d)
-  ruiseki[y] = sum(λ[0 : y + 1]) / sum(λ)
+    ruiseki = np.zeros(d)
+    ruiseki[y] = sum(λ[0: y + 1]) / sum(λ)
 
-  return z[:, y], kiyo[y], ruiseki[y]
-
-
+    return z[:, y], kiyo[y], ruiseki[y]
 
 
-deta = np.array([[8, 9, 4], [2, 5, 7], [8, 5, 6], [3, 5, 4], [7, 4, 9],
-       [4, 3, 4], [3, 6, 8],[6, 8, 2], [5, 4, 5], [6, 7, 6]])
+deta = np.array([[6, 9, 4], [2, 5, 7], [8, 5, 6], [3, 5, 4], [7, 4, 9],
+                 [4, 3, 4], [3, 6, 8], [6, 8, 2], [5, 4, 5], [6, 7, 6]])
 
 result1 = PCA(deta, 0)
 result2 = PCA(deta, 1)
